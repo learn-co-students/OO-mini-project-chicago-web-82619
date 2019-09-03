347ba60a6ec37fe30351e895a02cf64e316f3d34
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-09-03 16:40:14 -0500

    lab complete

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,23 @@
 require_relative '../config/environment.rb'
+require 'pry'
+r = Recipe.new('jumbolaya')
+q = Recipe.new('bulgogi')
 
+u = User.new('joe')
+k = User.new('kyle')
+
+o = Ingredient.new('onion')
+t = Ingredient.new('tomato')
+
+c1 = RecipeCard.new(u, r, 1, 10.0)
+c2 = RecipeCard.new(k, r, 2, 9.0)
+c3 = RecipeCard.new(k, q, 3, 8.0)
+
+i1 = RecipeIngredient.new(r, t)
+i2 = RecipeIngredient.new(q, o)
+
+a1 = Allergy.new(u, t)
+a2 = Allergy.new(k, o)
+a3 = Allergy.new(k, t)
 binding.pry
+true
